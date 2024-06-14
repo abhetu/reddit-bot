cd9e6de79e47dd698384d4beeae63b015cd7ee2e
Anubhav Bhetuwal <66554789+abhetu@users.noreply.github.com>
2024-06-13 20:08:20 -0500

    Add files via upload

---
+++ BotConfig.java
@@ -0,0 +1,7 @@
+public class BotConfig {
+    public static final String CLIENT_ID = "your_client_id";
+    public static final String CLIENT_SECRET = "your_client_secret";
+    public static final String REDDIT_USERNAME = "your_reddit_username";
+    public static final String REDDIT_PASSWORD = "your_reddit_password";
+    public static final String USER_AGENT = "YourBot/1.0 by YourUsername";
+}
